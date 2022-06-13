206d8836b49f3ef2a83716fd7af4dc8c50011bac
Ndubuisi Kalu <ndubisikalu19@gmail.com>
2022-06-13 10:29:46 +0100

    Finished responsiveness proceed to interactions

--- chatApp/index.js
+++ chatApp/index.js
@@ -1,16 +1,52 @@
 const menuItems = document.querySelectorAll('.menu-item');
+const messageNotifications= document.querySelector('#message-notifications');
+const messageBox = document.querySelector('.messages');
 
+console.log(menuItems)
 
-
-
-
-menuItems.forEach(menuItem => {
-    console.log(menuItem)
-    menuItem.addEventlistener('click', () => {
-
-        menuItem.classList.add('active');
+function removeActiveClass(){
+    menuItems.forEach((menuItem) =>{
+        menuItem.classList.remove('active');
     })
+}
+
+for(let item of menuItems){
+
+    item.addEventListener('click', () =>{
+        removeActiveClass();
+        item.classList.add('active');
+
+        if(item.id != 'notifications'){
+            document.querySelector('.notifications-popup').
+            style.display = 'none';
+            
+        }else{
+            document.querySelector('.notifications-popup').
+            style.display = 'block';
+            document.querySelector('.notification-count').
+            style.display = 'none';
+        }
+        
+    })
+}
+
+messageNotifications.addEventListener('click', () =>{
+    messageNotifications.querySelector('.notification-count').
+    style.display = 'none';
+    messageBox.style.boxShadow = '0 0 1rem var(--color-primary)';
+    setTimeout(() => {
+        messageBox.style.boxShadow = 'none';
+    }, 2000)
+
 })
+
+
+
+
+
+
+
+
 
 
 
